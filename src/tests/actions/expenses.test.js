60a6bbb989f737d04624a5ef06d54ea22649de95
PRANJAL SINGHAL <raj.singhal3d@gmail.com>
2020-09-28 00:37:50 +0530

    setup start edit expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,7 +1,8 @@
 import { 
     startAddExpense,
     addExpense, 
-    editExpense, 
+    editExpense,
+    startEditExpense, 
     removeExpense,
     startRemoveExpense, 
     setExpenses, 
@@ -52,6 +53,24 @@ test('should setup edit expense action object', () => {
         type: 'EDIT_EXPENSE',
         id:'456abc',
         updates: {note: 'Pranjal'}
+    });
+});
+
+test('should edit expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[2].id;
+    const updates = { amount: 21450 };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+        }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
     });
 });
 
